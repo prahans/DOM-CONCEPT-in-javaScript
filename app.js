586286bd9e4a8a71be7ae3d2b5cfef118363c495
anurag panuhar <165539474+prahans@users.noreply.github.com>
2026-01-22 19:04:23 +0545

    get

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-let oldImgs = document.getElementsByClassName("oldImg");
-for (let i = 0; i < oldImgs.length; i++) {
-  oldImgs[i].src = "assets/spiderman_img.png";
-}
+// let oldImgs = document.getElementsByClassName("oldImg");
+// for (let i = 0; i < oldImgs.length; i++) {
+//   oldImgs[i].src = "assets/spiderman_img.png";
+// }
+
+// document.all[8].innerText = "Anurag panuhar";
+
+// document.getElementById("mainImg").src = "assets/creation_1.png";
+
+// document.getElementsByTagName("p")[1].innerText = "abc";
+
+// console.dir(document.querySelector("p"));
+// console.dir(document.querySelector(".box a"));
+// console.dir(document.querySelectorAll(".box a"));
+// let demo = document.querySelectorAll(".box a");
+// for (let i = 0; i < demo.length; i++) {
+//   demo[i].innerHTML = "<i>hello world</i>";
+// }
+// console.dir(document.querySelector("#description"));
+
+// let heading = document.querySelector("h1");
+// heading.innerHTML = `<u>${heading.innerText}</u>`;
+
+// let img = document.querySelector("img");
+// console.log(img.getAttribute("id"));
+// img.setAttribute("id", "spiderman");
+// console.log(img.getAttribute("id"));
